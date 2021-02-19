ef6fd08995c232088ac4219515172d351e7e6502
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2021-02-19 19:20:17 +0100

    adjust file name for prior_fitting.py which creates sub files for cluster to fit priors

--- mcl_toolbox/mcrl_modelling/prior_fitting.py
+++ mcl_toolbox/mcrl_modelling/prior_fitting.py
@@ -20,7 +20,7 @@ for exp_num in exp_nums:
     for pid in pid_range:
             for model_index in model_nums:
                 args = [exp_num, model_index, optimization_criterion, pid]
-                sub_file = create_sub_file("prior_fit.py", args,  py_version = '3.8', logs = False, errs=False, outputs = False, process_arg = False, num_runs = 1,
+                sub_file = create_sub_file("fit_mcrl_models.py", args,  py_version = '3.8', logs = False, errs=False, outputs = False, process_arg = False, num_runs = 1,
                                             num_cpus=1)
                 submit_sub_file(sub_file, remove_on_submission=True, bid=2)
 
