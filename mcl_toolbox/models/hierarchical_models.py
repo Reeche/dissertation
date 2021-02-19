6f8b8bb0dbe6c56380d58c28cf4cfda0b2a8a10d
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2021-02-19 16:36:06 +0100

    fix import with missing top level module part in import

--- mcl_toolbox/models/hierarchical_models.py
+++ mcl_toolbox/models/hierarchical_models.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 import numpy as np
 import scipy as sp
-from mcl_toolbox.mcrl_modelling.global_vars import hierarchical_params
+from mcl_toolbox.global_vars import hierarchical_params
 from mcl_toolbox.models.base_learner import Learner
 from mcl_toolbox.utils.learning_utils import temp_sigmoid, rows_mean, \
     get_log_norm_pdf, get_log_norm_cdf
