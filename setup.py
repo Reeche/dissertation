bcaa02255744b77d4cb44b8f50599220e598e160
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2021-02-19 13:26:15 +0100

    add setup.py for package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mcl_toolbox',
+    version='',
+    packages=['mcl_toolbox', 'mcl_toolbox.env', 'mcl_toolbox.utils', 'mcl_toolbox.models', 'mcl_toolbox.mcrl_modelling',
+              'mcl_toolbox.computational_microscope'],
+    url='',
+    license='',
+    author='Ruiqi He, Yash Raj Jain',
+    author_email='',
+    description=''
+)
