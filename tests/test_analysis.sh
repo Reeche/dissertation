ef3af25850508c23cc9af9607dd93f9f3346b561
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2021-02-23 15:00:33 +0100

    change tests to be feasible

--- tests/test_analysis.sh
+++ tests/test_analysis.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd ..
-python mcl_toolbox/infer_participant_sequences.py
-python mcl_toolbox/infer_sequences.py increasing_variance training none
-python mcl_toolbox/fit_mcrl_models.py 1 v1.0 pseudo_likelihood 1
+python mcl_toolbox/infer_participant_sequences.py 1 F1 training
+python mcl_toolbox/infer_sequences.py F1 training
+python mcl_toolbox/fit_mcrl_models.py F1 1729 pseudo_likelihood 1 "{\"plotting\":True, \"optimization_params\" : {\"optimizer\":\"hyperopt\", \"num_simulations\": 2, \"max_evals\": 2}}"
