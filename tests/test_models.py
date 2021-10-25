7c52926528444aca41a8cf7c6d698ab639b2b655
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2021-10-25 12:24:22 +0200

    adjust name of variable so not hard coded "hundred"

--- tests/test_models.py
+++ tests/test_models.py
@@ -13,14 +13,14 @@ def test_models(
         _, _, _ = optimizer.optimize(criterion, **optimization_params)
     return None
 
-random_hundred_models = np.random.choice(range(6432), 300)
+random_models = np.random.choice(range(6432), 300)
 
 class TestModels(unittest.TestCase):
     @parameterized.expand([[model_index, {
             "optimizer": "hyperopt",
             "num_simulations": 1,
             "max_evals": 1,
-        }] for model_index in random_hundred_models])
+        }] for model_index in random_models])
     def test_mcrl_project_models(self, model_index, optimization_params):
         """
         Tests models to make sure we didn't break anything for MCRL Project
