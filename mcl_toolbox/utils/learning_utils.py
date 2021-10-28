68aaffa0289948cbfb5cd934ebb632f36bd0f445
Valkyrie Felso <valkyrie.felso@tuebingen.mpg.de>
2021-10-28 15:00:10 +0200

    add mer for "clicks_overlap" given error message

--- mcl_toolbox/utils/learning_utils.py
+++ mcl_toolbox/utils/learning_utils.py
@@ -1505,7 +1505,7 @@ def get_relevant_data(simulations_data, criterion):
     elif criterion in ["strategy_accuracy", "strategy_transition"]:
         return {"s": simulations_data["s"]}
     elif criterion in ["clicks_overlap"]:
-        return {"a": simulations_data["a"]}
+        return {"a": simulations_data["a"], "mer": simulations_data["mer"]}
     elif criterion in ["likelihood"]:
         if "loss" in simulations_data:
             return {"loss": simulations_data["loss"], "mer": simulations_data["mer"]}
