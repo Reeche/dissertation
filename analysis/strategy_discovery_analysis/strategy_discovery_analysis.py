ce90e78ae028a592aff271fbad17a370df8884d2
Ruiqi He <ruiqi.he@tuebingen.mpg.de>
2023-06-28 17:18:35 -0400

    * added analysis for strategy discovery to check whether the proporiton of participants learning the adaptive sequence is higher than chance
    Signed-off-by: Ruiqi He <ruiqi.he@tuebingen.mpg.de>

--- analysis/strategy_discovery_analysis/strategy_discovery_analysis.py
+++ analysis/strategy_discovery_analysis/strategy_discovery_analysis.py
@@ -4,8 +4,8 @@ import ast
 import matplotlib.pyplot as plt
 import pymannkendall as mk
 from scipy.stats import chisquare, sem, t
+import random
 
-data = pd.read_csv(f"../../data/human/strategy_discovery/mouselab-mdp.csv")
 
 
 ### create df
@@ -44,52 +44,29 @@ def create_df(data):
 
     # reindex trial count because some participants had to redo the quiz
     trial_number = list(range(1, 121))
-    trial_number = trial_number * len(pid_list) # (max(data["pid"]) - i)
+    trial_number = trial_number * len(pid_list)  # (max(data["pid"]) - i)
     click_df["trial"] = trial_number
 
     return pid_list, click_df
 
 
-pid_list, click_df = create_df(data)
 
-# optimal strategy is to first click inner nodes and then outer nodes
-immediate_nodes = [1, 5, 9]
-middle_nodes = [2, 6, 10]
-outer_nodes = [3, 4, 7, 8, 11, 12]
 
-for idx, row in click_df.iterrows():
-    # if row["number_of_clicks"] > 1 and row["number_of_clicks"] < 5: #relax to second most optimal strategy by < 6
-    #     row["clicks"] = [int(i) for i in row["clicks"]]
-        # if all but last one clicks are immediate nodes and last click is the outer node
-        # if all(item in immediate_nodes for item in row["clicks"][:-1]) and \
-        #         all(item in outer_nodes for item in row["clicks"][-1:]) and row["score"] > 34:
-    if row["score"] in [15, 14, 13]:
-    # if row["score"] in [35, 34, 33]:
-        click_df.at[idx, 'optimal_strategy'] = True
-    else:
-        click_df.at[idx, 'optimal_strategy'] = False
-
-# calculate proportion
-click_df = click_df[click_df["optimal_strategy"] == True]
-count = click_df.groupby("trial")["optimal_strategy"].count()
-count_prop = count/len(pid_list)
 
 def credible_interval(data, confidence=0.95):
     a = 1.0 * np.array(data)
     n = len(a)
     m, se = np.mean(a), sem(a)
-    h = se * t.ppf((1 + confidence) / 2., n-1)
-    return m, m-h, m+h
+    h = se * t.ppf((1 + confidence) / 2., n - 1)
+    return m, m - h, m + h
 
-print("CI: ", credible_interval(count_prop[-10:]))
 
 def chi_square_test(count_prop):
     # need actual count for goodness of fit test
     final_prop = count_prop.values[-1]
-    res = chisquare([final_prop * 100, (1-final_prop) * 100], f_exp=[0.15 * 100, 0.85 * 100])
+    res = chisquare([final_prop * 100, (1 - final_prop) * 100], f_exp=[0.15 * 100, 0.85 * 100])
     print("Chi^2 goodness of fit: ", res)
 
-chi_square_test(count_prop)
 
 def trend_test(count_prop):
     # increasing until when?
@@ -102,7 +79,6 @@ def trend_test(count_prop):
     print(result_all)
     return None
 
-# trend_test(count_prop)
 
 def plot_proportion(count_prop):
     plt.ylabel("Proportion of optimal strategy")
@@ -112,11 +88,74 @@ def plot_proportion(count_prop):
     ci = 0.1 * np.std(count_prop) / np.mean(count_prop)
     plt.plot(count_prop)
     x = list(range(0, len(count_prop)))
-    plt.fill_between(x, (count_prop-ci), (count_prop+ci), color='blue', alpha=0.1)
+    plt.fill_between(x, (count_prop - ci), (count_prop + ci), color='blue', alpha=0.1)
     plt.savefig("strategy_discovery.png")
     plt.show()
     # plt.close()
     return None
 
-# plot_proportion(count_prop)
 
+def classify_score(click_df):
+    # optimal strategy is to first click inner nodes and then outer nodes
+    immediate_nodes = [1, 5, 9]
+    middle_nodes = [2, 6, 10]
+    outer_nodes = [3, 4, 7, 8, 11, 12]
+
+    for idx, row in click_df.iterrows():
+        # if row["number_of_clicks"] > 1 and row["number_of_clicks"] < 5: #relax to second most optimal strategy by < 6
+        #     row["clicks"] = [int(i) for i in row["clicks"]]
+        # if all but last one clicks are immediate nodes and last click is the outer node
+        # if all(item in immediate_nodes for item in row["clicks"][:-1]) and \
+        #         all(item in outer_nodes for item in row["clicks"][-1:]) and row["score"] > 34:
+        if row["score"] in [15, 14, 13]:
+            # if row["score"] in [35, 34, 33]:
+            click_df.at[idx, 'optimal_strategy'] = True
+        else:
+            click_df.at[idx, 'optimal_strategy'] = False
+
+    # calculate proportion
+    click_df = click_df[click_df["optimal_strategy"] == True]
+    count = click_df.groupby("trial")["optimal_strategy"].count()
+    count_prop = count / len(pid_list)
+    return count_prop
+
+def classify_using_clicks(sequences):
+    # optimal strategy is to first click inner nodes and then outer nodes
+    immediate_nodes = [1, 5, 9]
+    middle_nodes = [2, 6, 10]
+    outer_nodes = [3, 4, 7, 8, 11, 12]
+
+    misclassified_sequences = []
+    for sequence in sequences:
+        if len(sequence) > 1 and len(sequence) < 6: #relax to second most optimal strategy by < 6
+            # if all but last one clicks are immediate nodes and last click is the outer node
+            if sequence[0] in immediate_nodes and sequence[-1] in outer_nodes:
+                misclassified_sequences.append(sequence)
+
+    print(len(misclassified_sequences) / len(sequences))
+    return len(misclassified_sequences) / len(sequences)
+
+def generate_random_sequences():
+    sequences = []
+    for _ in range(100000): #100.000 gives the same result as 1.000.000
+        length = random.randint(0, 12)
+        random_list = [random.randint(1, 12) for _ in range(length)]
+        sequences.append(random_list)
+    return sequences
+
+if __name__ == "__main__":
+    # data = pd.read_csv(f"../../data/human/strategy_discovery/mouselab-mdp.csv")
+    # pid_list, click_df = create_df(data)
+
+    # classify_sequences(click_df)
+
+    ## check how many randomly generated click sequences are wrongly classified as adaptive ones
+    random_sequences = generate_random_sequences()
+    misclassified_prop = classify_using_clicks(random_sequences)
+    res = chisquare([0.4 * 100, 0.6 * 100], f_exp = [misclassified_prop * 100, (1-misclassified_prop) * 100])
+    print(res)
+
+    # print("CI: ", credible_interval(count_prop[-10:]))
+    # trend_test(count_prop)
+    # chi_square_test(count_prop)
+    # plot_proportion(count_prop)
