acc03f4dd59816ebad1c0b6be020eccc52079f7a
Ruiqi He <ruiqi.he@tuebingen.mpg.de>
2023-12-08 14:16:50 +0100

    * added logistic regression analysis for proportion of adaptive strategies for MB vs MF experiment * Split the code for score related analysis and proportion related analysis into two different scripts
    Signed-off-by: Ruiqi He <ruiqi.he@tuebingen.mpg.de>

--- analysis/model_free_vs_model_based_analysis/proportion_analysis.py
+++ analysis/model_free_vs_model_based_analysis/proportion_analysis.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 import numpy as np
 import statsmodels.api as sm
-from statsmodels.formula.api import ols
 
 """
 Analyse whether 
@@ -46,33 +45,6 @@ def calculate_proportion_at_index(mydict, index):
     print("The proportion at the 15th trial: ", proportion)
     return proportion
 
-
-def compare_score(conditions, mf_clicked):
-    for condition in conditions:
-        # compare and plot and score of all the conditions: MF, MB, STROOP
-        df = pd.read_csv(f"../../data/human/{condition}/mouselab-mdp.csv")
-        # filter for data where "block" is "training"
-        df = df[df["block"] == "training"]
-        df = df[["pid", "trial_index", "score"]]
-
-        if condition == "mf":
-            df = df[df["pid"].isin(mf_clicked)]
-
-        # reset trial_index; each pid has trial 1-30
-        df["trial_index"] = df.groupby("pid").cumcount() + 1
-
-        # average score for all participants across trials
-        df_score = df.groupby(["trial_index"]).mean()
-
-        if condition == "mf":
-            x_values = list(range(30))
-            plt.plot(x_values, df_score["score"], label=condition)
-        else:
-            x_values = list(range(15, 30))
-            plt.plot(x_values, df_score["score"], label=condition)
-    return None
-
-
 def compare_adaptive_propotion(conditions):
     for condition in conditions:
         data_raw = pd.read_pickle(f"../../results/cm/inferred_strategies/{condition}_training/strategies.pkl")
@@ -105,41 +77,8 @@ def compare_adaptive_propotion(conditions):
     return None
 
 
-def plot_expected_score(conditions, mf_clicked):
-    for condition in conditions:
-        data = pd.DataFrame.from_dict(
-            pd.read_pickle(f"../../results/cm/inferred_strategies/{condition}_training/strategies.pkl"))
-
-        # filter data for mf_clicked
-        if condition == "mf":
-            data = data[mf_clicked]
-
-        score_mapping = pd.read_pickle(f"../../results/cm/strategy_scores/v1.0_strategy_scores.pkl")
-        # score_mapping start from 0 but inferred_strategies.pkl start from 1
-        strategy_df = data - 1
-        # replace strategy with score
-        strategy_df = strategy_df.replace(score_mapping)
-
-        # calculate average score for each trial
-        average = strategy_df.mean(axis=1)
-
-        # plot the average
-        if condition == "mf":
-            x_values = list(range(30))
-            plt.plot(x_values, average, label=condition.upper())
-        else:
-            x_values = list(range(15, 30))
-            plt.plot(x_values, average, label=condition.upper())
-    plt.legend()
-    plt.show()
-    plt.close()
-    return None
-
-
-def compare_expected_score(conditions, mf_clicked):
-    ### statistical test
-    # create a dataframe with score, trial and condition
-    exp_score_data = pd.DataFrame(columns=["exp_score", "trial", "condition"])
+def logistic_regression(conditions, mf_clicked, adaptive_strategies):
+    strategy_data = pd.DataFrame(columns=["trial", "condition"])
     for condition in conditions:
         data = pd.DataFrame.from_dict(
             pd.read_pickle(f"../../results/cm/inferred_strategies/{condition}_training/strategies.pkl"))
@@ -147,13 +86,7 @@ def compare_expected_score(conditions, mf_clicked):
         if condition == "mf":
             data = data[mf_clicked]
 
-        score_mapping = pd.read_pickle(f"../../results/cm/strategy_scores/v1.0_strategy_scores.pkl")
-        # score_mapping start from 0 but inferred_strategies.pkl start from 1
-        strategy_df = data - 1
-        # replace strategy with score
-        strategy_df = strategy_df.replace(score_mapping)
-
-        reshaped_df = pd.melt(strategy_df.reset_index(), id_vars='index', var_name='trial', value_name='exp_score')
+        reshaped_df = pd.melt(data.reset_index(), id_vars='index', var_name='trial', value_name='strategy')
         reshaped_df["condition"] = condition
 
         # if condition is MF, remove the first 15 trials from the dataframe
@@ -161,14 +94,15 @@ def compare_expected_score(conditions, mf_clicked):
             reshaped_df = reshaped_df[reshaped_df["index"] >= 15]
             reshaped_df["index"] = reshaped_df["index"] - 15
 
-        # create a dataframe with score, trial and condition
-        exp_score_data = exp_score_data._append(reshaped_df)
-    exp_score_data.columns = ["exp_score", "pid", "condition", "trial"]
+        # if strategy is in adaptive_strategies, replace with 1, else replace with 0
+        reshaped_df["strategy"] = reshaped_df["strategy"].apply(lambda x: 1 if x in adaptive_strategies else 0)
 
-    ## linear regression
-    # res = ols('exp_score ~ trial*condition', data=exp_score_data).fit()
-    res = ols('exp_score ~ trial*C(condition, Treatment("mf"))', data=exp_score_data).fit()
-    print(res.summary())
+        strategy_data = strategy_data._append(reshaped_df)
+    strategy_data.columns = ["pid", "condition", "trial", "strategy"]
+
+    # fit logistic regression
+    model = sm.GLM.from_formula("strategy ~ C(condition, Treatment('mf')) * trial", data=strategy_data, family=sm.families.Binomial()).fit()
+    print(model.summary())
     return None
 
 
@@ -184,10 +118,7 @@ if __name__ == "__main__":
                   125, 126, 127, 129, 130, 132, 134, 137, 138, 139, 141, 145, 146, 148, 149, 152, 156, 158, 159,
                   163, 167, 168, 172, 173, 175, 176, 179, 180, 182, 184, 186, 187, 189, 190, 191]
 
-    plot_expected_score(conditions, mf_clicked)
-    compare_expected_score(conditions, mf_clicked)
-
-
+    logistic_regression(conditions, mf_clicked, adaptive)
     ### Create a contingency table
     proportion_at_index = {}
 
---
+++ analysis/model_free_vs_model_based_analysis/score_analysis.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+import numpy as np
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+
+"""
+Analyse whether score and expected score differs across the conditions
+"""
+
+
+def compare_score(conditions, mf_clicked):
+    for condition in conditions:
+        # compare and plot and score of all the conditions: MF, MB, STROOP
+        df = pd.read_csv(f"../../data/human/{condition}/mouselab-mdp.csv")
+        # filter for data where "block" is "training"
+        df = df[df["block"] == "training"]
+        df = df[["pid", "trial_index", "score"]]
+
+        if condition == "mf":
+            df = df[df["pid"].isin(mf_clicked)]
+
+        # reset trial_index; each pid has trial 1-30
+        df["trial_index"] = df.groupby("pid").cumcount() + 1
+
+        # average score for all participants across trials
+        df_score = df.groupby(["trial_index"]).mean()
+
+        if condition == "mf":
+            x_values = list(range(30))
+            plt.plot(x_values, df_score["score"], label=condition)
+        else:
+            x_values = list(range(15, 30))
+            plt.plot(x_values, df_score["score"], label=condition)
+    return None
+
+
+def plot_expected_score(conditions, mf_clicked):
+    for condition in conditions:
+        data = pd.DataFrame.from_dict(
+            pd.read_pickle(f"../../results/cm/inferred_strategies/{condition}_training/strategies.pkl"))
+
+        # filter data for mf_clicked
+        if condition == "mf":
+            data = data[mf_clicked]
+
+        score_mapping = pd.read_pickle(f"../../results/cm/strategy_scores/v1.0_strategy_scores.pkl")
+        # score_mapping start from 0 but inferred_strategies.pkl start from 1
+        strategy_df = data - 1
+        # replace strategy with score
+        strategy_df = strategy_df.replace(score_mapping)
+
+        # calculate average score for each trial
+        average = strategy_df.mean(axis=1)
+
+        # plot the average
+        if condition == "mf":
+            x_values = list(range(30))
+            plt.plot(x_values, average, label=condition.upper())
+        else:
+            x_values = list(range(15, 30))
+            plt.plot(x_values, average, label=condition.upper())
+
+        # add 95% confidence interval
+        # calculate standard error
+        standard_error = strategy_df.sem(axis=1)
+        # calculate upper and lower bound
+        upper_bound = average + 1.96 * standard_error
+        lower_bound = average - 1.96 * standard_error
+        # plot the upper and lower bound
+        plt.fill_between(x_values, upper_bound, lower_bound, alpha=0.2)
+
+    plt.xlabel("Trial")
+    plt.ylabel("Expected score")
+    plt.legend()
+    plt.show()
+    plt.close()
+    return None
+
+
+def compare_expected_score(conditions, mf_clicked):
+    ### statistical test
+    # create a dataframe with score, trial and condition
+    exp_score_data = pd.DataFrame(columns=["exp_score", "trial", "condition"])
+    for condition in conditions:
+        data = pd.DataFrame.from_dict(
+            pd.read_pickle(f"../../results/cm/inferred_strategies/{condition}_training/strategies.pkl"))
+        # filter data for mf_clicked
+        if condition == "mf":
+            data = data[mf_clicked]
+
+        score_mapping = pd.read_pickle(f"../../results/cm/strategy_scores/v1.0_strategy_scores.pkl")
+        # score_mapping start from 0 but inferred_strategies.pkl start from 1
+        strategy_df = data - 1
+        # replace strategy with score
+        strategy_df = strategy_df.replace(score_mapping)
+
+        reshaped_df = pd.melt(strategy_df.reset_index(), id_vars='index', var_name='trial', value_name='exp_score')
+        reshaped_df["condition"] = condition
+
+        # if condition is MF, remove the first 15 trials from the dataframe
+        if condition == "mf":
+            reshaped_df = reshaped_df[reshaped_df["index"] >= 15]
+            reshaped_df["index"] = reshaped_df["index"] - 15
+
+        # create a dataframe with score, trial and condition
+        exp_score_data = exp_score_data._append(reshaped_df)
+    exp_score_data.columns = ["exp_score", "pid", "condition", "trial"]
+
+    ## linear regression
+    # res = ols('exp_score ~ trial*condition', data=exp_score_data).fit()
+    res = ols('exp_score ~ trial*C(condition, Treatment("mf"))', data=exp_score_data).fit()
+    print(res.summary())
+    return None
+
+
+if __name__ == "__main__":
+    adaptive = [65, 64, 24, 21, 63, 43, 17, 16, 57, 59, 88, 54, 4, 31, 26, 82, 37, 48, 50, 85, 76, 18, 84, 45, 11, 6, 7,
+                29, 80, 36, 67, 12, 87, 60, 5, 10, 49, 42, 72, 51, 2, 71, 13, 40, 14, 15, 78, 56]
+    # maladaptive = [22, 53, 39, 28, 30, 23, 66, 70, 74]
+    # others = [33, 44, 27, 79, 69, 34, 61, 73, 32]
+    conditions = ["mf", "mb", "stroop"]
+
+    mf_clicked = [3, 5, 9, 10, 13, 15, 23, 25, 28, 30, 32, 33, 36, 37, 41, 45, 46, 52, 56, 58, 59, 62, 63, 66, 68,
+                  69, 72, 74, 76, 78, 82, 84, 86, 89, 91, 93, 94, 96, 98, 100, 102, 104, 108, 111, 115, 116, 124,
+                  125, 126, 127, 129, 130, 132, 134, 137, 138, 139, 141, 145, 146, 148, 149, 152, 156, 158, 159,
+                  163, 167, 168, 172, 173, 175, 176, 179, 180, 182, 184, 186, 187, 189, 190, 191]
+
+    plot_expected_score(conditions, mf_clicked)
+    compare_expected_score(conditions, mf_clicked)
